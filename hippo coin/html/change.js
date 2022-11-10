04fe9ded698083f7ed197427b3e018ad7632e7ff
owenlikecoding <95719237+owenlikecoding@users.noreply.github.com>
2022-11-09 19:04:05 -0500

    41% did this to me

---
+++ hippo coin/html/change.js
@@ -0,0 +1,4 @@
+var bgColor = window.localStorage.getItem('bgColor');
+var textColor = window.localStorage.getItem('textColor');
+document.body.style.backgroundColor = bgColor;
+document.body.style.color = textColor;
